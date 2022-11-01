40248f6f0eae677b3f6ffc15d95b2a8800389c34
scottzyang <scottzyang@gmail.com>
2022-11-01 10:48:38 -0500

    Add form to choose_froyo route

--- app.py
+++ app.py
@@ -17,7 +17,13 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return """
+    <form action="/froyo_results" method="GET">
+        What is your favorite Fro-Yo flavor? <br/>
+        <input type="text" name="flavor"<br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
 @app.route('/froyo_results')
 def show_froyo_results():
