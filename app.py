3ecb17c4730976d0b8f4f177bc481b036c2148b0
scottzyang <scottzyang@gmail.com>
2022-11-01 12:55:53 -0500

    Add template for froyo

--- app.py
+++ app.py
@@ -17,23 +17,17 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    return """
-    <form action="/froyo_results" method="GET">
-        What is your favorite Fro-Yo flavor? <br/>
-        <input type="text" name="flavor"><br/>
-        What toppings? <br/>
-        <input type="text" name="toppings"> <br/>
-        <input type="submit" value="Submit!">
-    </form>
-    """
+    return render_template('/froyo_form.html')
 
 @app.route('/froyo_results')
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
-    users_froyo_flavor = request.args.get('flavor')
-    users_toppings = request.args.get('toppings')
-    return f'You ordered {users_froyo_flavor} flavored Fro-Yo with {users_toppings} as toppings!'
-
+    context = {
+    'users_froyo_flavor': request.args.get('flavor'),
+    'users_toppings': request.args.get('toppings')
+    }
+    return render_template('froyo_results.html', **context)
+    
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
